e36ed291b7730b98aa93aa6975aab4a19374e43b
Minhggg <minhnnhe180467@fpt.edu.vn>
2026-02-27 10:51:43 +0700

    refactor:code base prepare for update i18N

---
+++ src/i18n.ts
@@ -0,0 +1,24 @@
+import i18next from "i18next";
+import { initReactI18next } from "react-i18next";
+import en from "./locales/en.json";
+import vi from "./locales/vi.json";
+
+i18next
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      vi: {
+        translation: vi,
+      },
+    },
+    lng: "vi", 
+    fallbackLng: "vi",
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18next;
